b8ff9bdb8b69af370f53153ac9ac600614f9a29a
tifapp <tifapppublic@gmail.com>
2023-05-20 23:04:08 -0700

    Switched some queries to use objects instead of arrays for parameterization

--- sqlhandler.ts
+++ sqlhandler.ts
@@ -38,13 +38,8 @@ export const getAllUsers = async (req: UserList) => {
 };
 
 export const getUserById = async (req: User) => {
-  let SQL = "SELECT * FROM User WHERE userId = ?";
-  const sqlparams = [];
-  console.log(req.userId);
-  sqlparams.push(req.userId);
-  console.log(sqlparams);
-  const results = await conn.execute(SQL, sqlparams);
-  console.log(results);
+  let SQL = "SELECT * FROM User WHERE userId = :userId";
+  const results = await conn.execute(SQL, req);
   conn.refresh();
   return {
     statusCode: 200,
@@ -56,31 +51,23 @@ export const getUserById = async (req: User) => {
 };
 
 export const updateUser = async (req: any) => {
+  let body = JSON.parse(req.body);
+  body.userId = req.userId;
   let UPDATE = "UPDATE User ";
+  let keys = Object.keys(body);
+  
   let SET = "SET ";
-  let sqlparams = [];
-  //console.log(req.params.userId);
-  let parse = JSON.parse(req.body);
-  //console.log("parse :" + parse);
-  let length = Object.keys(parse).length;
-  console.log("length :" + length);
-  for (var key in parse) {
-    if (length > 1) {
-      SET = SET + key + " = ?,";
-      sqlparams.push(parse[key]);
-    } else {
-      SET = SET + key + " = ? ";
-      sqlparams.push(parse[key]);
+  keys.forEach((key, index) => {
+    SET += ` ${key} = :${key}`;
+    if (index < keys.length - 1) {
+      SET += ", ";
     }
-    length = length - 1;
-    //console.log(key)
-    //console.log(parse[key])
-  }
+  })
 
-  UPDATE = UPDATE + SET + " WHERE userId = ?";
-  sqlparams.push(req.params.userId);
-  const results = await conn.execute(UPDATE, sqlparams);
-  console.log(results);
+  UPDATE += ` ${SET} WHERE userId = :userId`;
+  
+  const results = await conn.execute(UPDATE, body);
+  
   conn.refresh();
   return {
     statusCode: 200,
@@ -92,32 +79,25 @@ export const updateUser = async (req: any) => {
 };
 
 export const createUser = async (req: any) => {
+  let body = JSON.parse(req.body);
+  body.userId = req.userId;
   let INSERT = "INSERT INTO User (userId, ";
   let VALUES = "VALUES (";
-  let sqlparams = [];
-  sqlparams.push(req.userId);
-  let parse = JSON.parse(req.body);
-  let length = Object.keys(parse).length;
-  console.log("length :" + length);
-  for (var key in parse) {
-    if (length > 1) {
-      INSERT += key + ", ";
-      sqlparams.push(parse[key]);
-      VALUES += "?, ";
-    } else {
-      INSERT += key;
-      sqlparams.push(parse[key]);
-      VALUES += "?";
+  let keys = Object.keys(body);
+
+  keys.forEach((key, index) => {
+    INSERT += key;
+    VALUES += ` :${key}`;
+    if (index < keys.length - 1) {
+      INSERT += ", ";
+      VALUES += ", ";
     }
-    length = length - 1;
-    //console.log(key)
-    //console.log(parse[key])
-  }
+  })
+  
   INSERT += ") " + VALUES + ") ";
-  console.log(INSERT);
-  console.log(sqlparams);
-  const results = await conn.execute(INSERT, sqlparams);
-  console.log(results);
+  
+  const results = await conn.execute(INSERT, body);
+  
   conn.refresh();
   return {
     statusCode: 200,
@@ -133,26 +113,22 @@ export const createUser = async (req: any) => {
 // 1) add check for if user blocked owner / owner blocked user
 // 2) ???
 export const getEvents = async (req: any) => {
-  let sqlparams = [];
-  sqlparams.push(req.userId);
-  sqlparams.push(req.body.lon);
-  sqlparams.push(req.body.lat);
-  sqlparams.push(req.userId);
-  sqlparams.push(req.userId);
+  let body = JSON.parse(req.body);
+  body.userId = req.userId;
 
   let SELECT =
     "SELECT E.name AS event_name, E.description, E.eventId, E.ownerId, E.startDate, E.endDate, COUNT(A.userId) AS attendee_count, CASE WHEN F.user IS NOT NULL THEN 1 ELSE 0 END AS is_friend ";
   let FROM =
-    "FROM Event E JOIN Location L ON E.eventId = L.eventId LEFT JOIN eventAttendance A ON E.eventId = A.eventId LEFT JOIN Friends F ON E.ownerId = F.friend AND F.user = ? ";
+    "FROM Event E JOIN Location L ON E.eventId = L.eventId LEFT JOIN eventAttendance A ON E.eventId = A.eventId LEFT JOIN Friends F ON E.ownerId = F.friend AND F.user = :userId ";
   let WHERE =
-    "WHERE ST_Distance_Sphere(POINT( ?, ?), POINT(lon, lat)) < 3100 AND E.endDate > NOW() ";
+    "WHERE ST_Distance_Sphere(POINT(:lon, :lat), POINT(lon, lat)) < 3100 AND E.endDate > NOW() ";
   let REST =
-    "AND ? NOT IN (SELECT blocked FROM blockedUsers WHERE user = E.ownerId AND blocked = ?) GROUP BYE.eventId;";
+    "AND :userId NOT IN (SELECT blocked FROM blockedUsers WHERE user = E.ownerId AND blocked = :userId) GROUP BYE.eventId;";
 
   let SQL = SELECT + FROM + WHERE + REST;
 
-  const results = await conn.execute(SQL, sqlparams);
-  console.log(results);
+  const results = await conn.execute(SQL, body);
+  
   conn.refresh();
   return {
     statusCode: 200,
