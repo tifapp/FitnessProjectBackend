3bd7cf3d6ee306a68c66dbb56f7a7c84e3275c2b
Matthew Hayes <matthewha853@gmail.com>
2023-05-25 16:30:45 -0700

    Handle case when user already exists

--- users.ts
+++ users.ts
@@ -1,7 +1,8 @@
-import express, { Application } from "express";
-import { SQLExecutable } from "./dbconnection";
+import express from "express";
+import { SQLExecutable, hasResults } from "./dbconnection";
 import { ServerEnvironment } from "./env";
 import { Connection } from "@planetscale/database";
+import { Result } from "./utils";
 
 /**
  * Creates routes related to user operations.
@@ -13,10 +14,14 @@ export const createUserRouter = (environment: ServerEnvironment) => {
   const router = express.Router();
 
   router.post("/", async (req, res) => {
-    await insertUser(environment.conn, {
+    const result = await registerUser(environment.conn, {
       id: res.locals.selfId,
       ...req.body,
     });
+
+    if (result.status === "error") {
+      return res.status(400).json({ error: result.data });
+    }
     return res.status(201).json({ id: res.locals.selfId });
   });
 
@@ -27,6 +32,25 @@ export type InsertUserRequest = {
   id: string;
   name: string;
   handle: string;
+};
+
+export type RegisterUserError = "user-already-exists" | "non-unique-handle";
+
+const registerUser = async (
+  conn: Connection,
+  request: InsertUserRequest
+): Promise<Result<undefined, RegisterUserError>> => {
+  return await conn.transaction(async (tx) => {
+    if (await userWithIdExists(tx, request.id)) {
+      return { status: "error", data: "user-already-exists" };
+    }
+    await insertUser(tx, request);
+    return { status: "success" };
+  });
+};
+
+const userWithIdExists = async (conn: SQLExecutable, id: string) => {
+  return await hasResults(conn, "SELECT * FROM user WHERE id = :id", { id });
 };
 
 /**
