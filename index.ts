6bbe588df0c93274f41a343a0626e626ac7d22bb
Matthew Hayes <matthewha853@gmail.com>
2023-05-25 00:24:53 -0700

    Move auth to a separate file

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import { app } from "./app";
 
 import awsServerlessExpress from "@vendia/serverless-express";
+import { addCognitoTokenVerification } from "./auth";
 
+addCognitoTokenVerification(app);
 export const handler = awsServerlessExpress({ app });
