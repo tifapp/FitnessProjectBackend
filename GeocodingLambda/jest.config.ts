b25a36b77f1d02e227ea3a91f4353399b208c1b7
tifapp <tifapppublic@gmail.com>
2023-07-07 00:52:10 -0700

    Geocoding lambda with retry

---
+++ GeocodingLambda/jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+  preset: "ts-jest/presets/js-with-ts-esm",
+  testEnvironment: "node",
+  clearMocks: true,
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+  },
+  transformIgnorePatterns: ["/node_modules/(?!@planetscale)"],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
