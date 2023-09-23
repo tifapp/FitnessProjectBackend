96944b9410290d1714000e18a024ca0e966da378
tifapp <tifapppublic@gmail.com>
2023-09-22 21:05:58 -0700

    Add tests for generateUserHandle

--- APILambda/user/generateUserHandle.ts
+++ APILambda/user/generateUserHandle.ts
@@ -5,13 +5,14 @@ import { userWithHandleExists } from "./SQL"
 const generateNumericHash = (input: string) => {
   const hash = crypto.createHash("sha1").update(input).digest("hex")
   // Convert hex hash to a BigInt and then take modulus to get a fixed size numeric hash
-  const numericHash = BigInt(`0x${hash}`) % BigInt(1e5) // using a 5-digit number for simplicity
-  return numericHash.toString().padStart(5, "0")
+  const numericHash = BigInt(`0x${hash}`) % BigInt(1e4) // using a 5-digit number for simplicity
+  return numericHash.toString().padStart(4, "0")
 }
 
 export const generateUniqueUsername = async (conn: SQLExecutable, name: string, maxRetries = 3): Promise<string> => {
-  const cleanedName = name.replace(/[^a-zA-Z0-9]/g, "").toLowerCase()
-  const baseName = cleanedName.substr(0, 10)
+  let cleanedName = name.replace(/[^a-zA-Z0-9]/g, "").toLowerCase()
+  cleanedName = cleanedName.length ? cleanedName : "idk"
+  const baseName = cleanedName.substr(0, 11)
 
   let retries = 0
   let potentialUsername = `${baseName}${generateNumericHash(`${cleanedName}${Date.now()}`)}`
