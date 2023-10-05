abedbd55fbeca554cd2e7d7c872a6c7dd3dd9fcb
tifapp <tifapppublic@gmail.com>
2023-10-04 19:43:06 -0700

    Added autocomplete endpoint to swagger + reverted req

--- APILambda/index.ts
+++ APILambda/index.ts
@@ -1,32 +1,12 @@
-import awsServerlessExpress, { getCurrentInvoke } from "@vendia/serverless-express"
-import { Express } from "express"
+import awsServerlessExpress from "@vendia/serverless-express"
 import { addRoutes, createApp } from "./app.js"
 import { addCognitoTokenVerification } from "./auth.js"
 import { conn } from "./dbconnection.js"
 import { ServerEnvironment } from "./env.js"
 
-const addEventToRequest = (app: Express) => {
-  app.use((req, res, next) => {
-    const { event } = getCurrentInvoke()
-
-    // TODO: Find better solution
-    for (const [key, value] of Object.entries(event)) {
-      Object.defineProperty(req, key, {
-        value,
-        writable: true,
-        enumerable: true,
-        configurable: true
-      })
-    } // WARN: ensure fields in "event" do not overwrite fields in "req"
-
-    next()
-  })
-}
-
 const env: ServerEnvironment = { conn, environment: "prod" }
 
 const app = createApp()
-addEventToRequest(app)
 addCognitoTokenVerification(app, env) // only apply to specific routes
 addRoutes(app, env)
 
