499f9ba2677f07b7c20cee89814d7ad298fcedb0
tifapp <tifapppublic@gmail.com>
2023-10-14 02:02:43 -0700

    no message

--- APILambda/deleteTestUsers.ts
+++ APILambda/deleteTestUsers.ts
@@ -1,6 +1,7 @@
 /* eslint-disable import/extensions */ // todo: allow ts imports here
 
 export default async (): Promise<void> => {
+  console.log(process.memoryUsage())
   if (process.env.NODE_ENV === "staging") {
     // delete cognito users
   }
--- APILambda/test/testApp.ts
+++ APILambda/test/testApp.ts
@@ -1,11 +1,11 @@
 import { addRoutes, createApp } from "../app.js"
 import { addCognitoTokenVerification } from "../auth.js"
 import { conn } from "../dbconnection.js"
-import { EnvironmentType, ServerEnvironment } from "../env.js"
+import { ServerEnvironment } from "../env.js"
 
 export const testEnv: ServerEnvironment = {
   // use env vars
-  environment: process.env.NODE_ENV as EnvironmentType ?? "dev",
+  environment: process.env.NODE_ENV === "staging" ? "staging" : "dev",
   conn
 }
 
