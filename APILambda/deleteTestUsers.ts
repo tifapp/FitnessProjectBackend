99297e9c42fe35ce588523f895bfa0b5f705f40f
tifapp <tifapppublic@gmail.com>
2023-10-16 19:25:43 -0700

    Configure test env

--- APILambda/deleteTestUsers.ts
+++ APILambda/deleteTestUsers.ts
@@ -1,8 +1,11 @@
 /* eslint-disable import/extensions */ // todo: allow ts imports here
+import dotenv from "dotenv"
+
+dotenv.config()
 
 export default async (): Promise<void> => {
   console.log(process.memoryUsage())
-  if (process.env.NODE_ENV === "staging") {
+  if (process.env.TEST_ENV === "staging") {
     // delete cognito users
   }
 }
