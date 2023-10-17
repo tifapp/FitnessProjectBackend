bb4e2b3b8e8c4fe958eb26ea70c58196596a93f4
tifapp <tifapppublic@gmail.com>
2023-10-16 21:20:19 -0700

    Add benchmarking for staging tests

--- APILambda/test/testApp.ts
+++ APILambda/test/testApp.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv"
+import { Application } from "express"
 import { addRoutes, createApp } from "../app.js"
 import { addCognitoTokenVerification } from "../auth.js"
 import { conn } from "../dbconnection.js"
@@ -12,6 +13,17 @@ export const testEnv: ServerEnvironment = {
   conn
 }
 
+export const addBenchmarking = (app: Application) => {
+  app.use((req, res, next) => {
+    const start = Date.now()
+    res.on("finish", () => {
+      const duration = Date.now() - start
+      console.log(`[${req.method}] ${req.originalUrl} took ${duration}ms`)
+    })
+    next()
+  })
+}
+
 /**
  * Creates a test environment application.
  *
@@ -21,6 +33,7 @@ export const testApp = process.env.TEST_ENV === "staging"
   ? process.env.API_ENDPOINT
   : (() => {
     const app = createApp()
+    addBenchmarking(app)
     addCognitoTokenVerification(app, testEnv)
     addRoutes(app, testEnv)
     return app
