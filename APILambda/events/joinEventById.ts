ad8406a93bdc5d62d6e3f7a60f2bf394bdd42d6a
suryatiru <sutiruma@ucsc.edu>
2023-11-23 00:53:05 -0800

    Fixed failing test
    - Created Query Result type to return the insert id and rowsAffected
    - executeAndReturnQueryResult will return those fields
    - execute method however only now returns the rows from the query
    - With the executeAndReturnQueryResult, we can determine if a user joined the event for the first time or tried to join the same event more than once since we can use the rowsAffected prop to give us that information

--- APILambda/events/joinEventById.ts
+++ APILambda/events/joinEventById.ts
@@ -1,4 +1,4 @@
-import { SQLExecutable, conn, failure } from "TiFBackendUtils"
+import { SQLExecutable, conn, failure, success } from "TiFBackendUtils"
 import { z } from "zod"
 import { ServerEnvironment } from "../env.js"
 import { ValidatedRouter } from "../validation.js"
@@ -6,34 +6,42 @@ import { checkChatPermissionsTransaction } from "./getChatToken.js"
 import { getEventById } from "./getEventById.js"
 import { isUserNotBlocked } from "./sharedSQL.js"
 
-const joinEventSchema = z
-  .object({
-    eventId: z.string()
-  })
+const joinEventSchema = z.object({
+  eventId: z.string()
+})
 
-const joinEvent = (
-  conn: SQLExecutable,
-  userId: string,
-  eventId: number) => {
+const joinEvent = (conn: SQLExecutable, userId: string, eventId: number) => {
   return conn.transaction((tx) =>
     getEventById(conn, eventId)
-      .flatMapSuccess(
-        (event) => isUserNotBlocked(tx, event.hostId, userId).withSuccess(event)
-      ).flatMapSuccess(
-        (event) => new Date() < event.endTimestamp
-          ? addUserToAttendeeList(conn, userId, eventId)
-          : failure("event-has-ended")
-      ).flatMapSuccess(
-        () => checkChatPermissionsTransaction(eventId, userId)
+      .flatMapSuccess((event) =>
+        isUserNotBlocked(tx, event.hostId, userId).withSuccess(event)
       )
+      .flatMapSuccess((event) =>
+        new Date() < event.endTimestamp
+          ? addUserToAttendeeList(tx, userId, eventId).flatMapSuccess(
+            ({ rowsAffected }) =>
+              rowsAffected > 0 ? success(201) : success(200)
+          )
+          : failure("event-has-ended")
+      )
+      .flatMapSuccess((status) => {
+        return success({
+          ...checkChatPermissionsTransaction(eventId, userId),
+          status
+        })
+      })
   )
 }
 
-const addUserToAttendeeList = (conn: SQLExecutable, userId: string, eventId: number) => conn.queryResults(
-  `INSERT INTO eventAttendance (userId, eventId, joinDate)
-    VALUES (:userId, :eventId, CURRENT_TIMESTAMP())`,
-  { userId, eventId }
-)
+const addUserToAttendeeList = (
+  conn: SQLExecutable,
+  userId: string,
+  eventId: number
+) =>
+  conn.queryResult(
+    "INSERT IGNORE INTO eventAttendance (userId, eventId) VALUES (:userId, :eventId)",
+    { userId, eventId }
+  )
 
 /**
  * Join an event given an event id.
@@ -45,10 +53,24 @@ export const joinEventRouter = (
   router: ValidatedRouter
 ) => {
   /**
-    * Join an event
-    */
-  router.postWithValidation("/join/:eventId", { pathParamsSchema: joinEventSchema }, (req, res) =>
-    joinEvent(conn, res.locals.selfId, Number(req.params.eventId))
-      .mapFailure((error) => res.status(error === "event-not-found" ? 404 : error === "user-is-blocked" || error === "event-has-ended" ? 403 : 500).json({ error }))
-      .mapSuccess((event) => res.status(200).json(event)))
+   * Join an event
+   */
+  router.postWithValidation(
+    "/join/:eventId",
+    { pathParamsSchema: joinEventSchema },
+    (req, res) =>
+      joinEvent(conn, res.locals.selfId, Number(req.params.eventId))
+        .mapFailure((error) =>
+          res
+            .status(
+              error === "event-not-found"
+                ? 404
+                : error === "user-is-blocked" || error === "event-has-ended"
+                  ? 403
+                  : 500
+            )
+            .json({ error })
+        )
+        .mapSuccess((event) => res.status(event.status).json(event))
+  )
 }
