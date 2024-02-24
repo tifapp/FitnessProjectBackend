16ad53c561969f79c293bc1a6a2ba7f5fc4b3572
tifapp <tifapppublic@gmail.com>
2024-02-24 00:50:50 -0800

    remove stagingIndex dependency on devindex

--- APILambda/stagingIndex.ts
+++ APILambda/stagingIndex.ts
@@ -1,18 +1,19 @@
 import awsServerlessExpress from "@vendia/serverless-express"
 import { LocationCoordinate2D, invokeAWSLambda } from "TiFBackendUtils"
-import { addBenchmarking, addRoutes, createApp } from "../app.js"
-import { addCognitoTokenVerification } from "../auth.js"
-import { ServerEnvironment } from "../env.js"
-import { addEventToRequest } from "../index.js"
-import { setProfileCreatedAttribute } from "../user/createUser/setCognitoAttribute.js"
-import { testEnv } from "./devIndex.js"
+import { addBenchmarking, addRoutes, createApp } from "./app.js"
+import { addCognitoTokenVerification } from "./auth.js"
+import { ServerEnvironment } from "./env.js"
+import { addEventToRequest } from "./index.js"
+import { setProfileCreatedAttribute } from "./user/createUser/setCognitoAttribute.js"
 
+// do not import anything from ./test here, wont be accessible in deployment
 const stagingEnv: ServerEnvironment = {
-  ...testEnv,
   setProfileCreatedAttribute,
   callGeocodingLambda: (location: LocationCoordinate2D) =>
     invokeAWSLambda("geocodingPipeline", location),
-  environment: "staging"
+  environment: "staging",
+  maxArrivals: 4,
+  eventStartWindowInHours: 1
 }
 
 const app = createApp()
