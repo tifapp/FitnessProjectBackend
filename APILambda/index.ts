38346f514d1abed615b00f3c906a5bda76c95a7b
tifapp <tifapppublic@gmail.com>
2024-02-24 00:56:17 -0800

    fix staging index imports

--- APILambda/index.ts
+++ APILambda/index.ts
@@ -1,43 +1,14 @@
-import awsServerlessExpress, {
-  getCurrentInvoke
-} from "@vendia/serverless-express"
+import awsServerlessExpress from "@vendia/serverless-express"
 import {
   LocationCoordinate2D,
   invokeAWSLambda
 } from "TiFBackendUtils"
-import express, { Express } from "express"
 import { addBenchmarking, addRoutes, createApp } from "./app.js"
 import { addCognitoTokenVerification } from "./auth.js"
 import { ServerEnvironment } from "./env.js"
 import { addErrorReporting } from "./errorReporting.js"
+import { addEventToRequest } from "./serverlessMiddleware.js"
 import { setProfileCreatedAttribute } from "./user/createUser/setCognitoAttribute.js"
-
-export const addEventToRequest = (app: Express) => {
-  app.use((req, res, next) => {
-    const { event } = getCurrentInvoke()
-
-    // TODO: Find better solution
-    for (const [key, value] of Object.entries(event)) {
-      Object.defineProperty(req, key, {
-        value,
-        writable: true,
-        enumerable: true,
-        configurable: true
-      })
-    } // WARN: ensure fields in "event" do not overwrite fields in "req"
-
-    Object.defineProperty(req, "body", {
-      value: JSON.parse(req.body),
-      writable: true,
-      enumerable: true,
-      configurable: true
-    })
-
-    next()
-  })
-  app.use(express.json())
-  app.use(express.urlencoded({ extended: true }))
-}
 
 const env: ServerEnvironment = {
   environment: "prod",
