f340eaf2348664cdbf5ad93601d22d1789944e59
tifapp <tifapppublic@gmail.com>
2024-02-25 15:01:33 -0800

    Add a script to generate ts files from planetscale db

--- TiFBackendUtils/index.ts
+++ TiFBackendUtils/index.ts
@@ -1,4 +1,5 @@
 export * from "./AWS/index.js"
+export * from "./Planetscale/entities.js"
 export * from "./Retryable/utils.js"
 export * from "./SQLExecutable/index.js"
 export * from "./UserHandle.js"
