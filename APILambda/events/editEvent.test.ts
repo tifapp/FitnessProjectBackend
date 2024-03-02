2bf8d08de28e6b3c81a4491de64e7b3ed7bb33dc
TCbacon <henrock135@gmail.com>
2024-03-01 21:24:52 -0800

    Add placeholder test to edit event file

--- APILambda/events/editEvent.test.ts
+++ APILambda/events/editEvent.test.ts
@@ -1,1 +1,7 @@
-//TODO implement test for preventing users from editing an event that ended
+//TODO implement tests for editing an event
+describe("End/cancel event tests", () => {
+  it("edit event test placeholder", async () => {
+    const placeholder = "placeholder"
+    expect(placeholder).toBeTruthy()
+  })
+})
