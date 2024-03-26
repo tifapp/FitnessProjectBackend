81846b72fc150a02d7cd2f6b87107acb191371c5
TCbacon <henrock135@gmail.com>
2024-03-25 19:27:07 -0700

    Add node script to promote lambda to staging

---
+++ APILambda/lambdatostaging/lambdatostaging.js
@@ -0,0 +1,39 @@
+import npmExecHelper from './lambdatostaginghelper.js';
+
+const npmExecInstance = new npmExecHelper();
+
+const handleError = (output, errorMessage, dir = "../TiFBackendUtils") => {
+    if (output !== 0) {
+        console.error(errorMessage);
+        console.log(`deleting dist folder content from ${dir}/dist`);
+        cleanUp();
+        process.exit(1);
+    }
+};
+
+const cleanUp = () => {
+    npmExecInstance.revertToPackageJsonName();
+    npmExecInstance.deleteDistBuildFolders();
+};
+
+cleanUp();
+
+const cleanInstallOutput = npmExecInstance.cleanInstall();
+handleError(cleanInstallOutput, "Failed to install backend utils dependencies.");
+
+const buildOutput = npmExecInstance.runBuild();
+handleError(buildOutput, "Failed to build backend utils js.");
+
+const runPackage = npmExecInstance.runPackageShellJS();
+handleError(runPackage, "Failed to package.");
+
+const runInstallAPILambdaDependencies = npmExecInstance.runInstallAPILambdaDependencies();
+handleError(runInstallAPILambdaDependencies, "Failed to install API lambda dependencies.");
+
+const runBuildAPILambdaZip = npmExecInstance.runBuildAPILambdaZip();
+handleError(runBuildAPILambdaZip, "Failed to build APILambda zip.");
+
+const deployLambdaToStaging = await npmExecInstance.deployLambdaToStagingAWSSdk();
+handleError(deployLambdaToStaging, "Failed to deploy lambda to staging");
+
+cleanUp();
