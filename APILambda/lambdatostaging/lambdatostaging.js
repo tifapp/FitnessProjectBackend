111418d0ff90c4537a2c0c099d10a33d0e4507f9
tifapp <tifapppublic@gmail.com>
2024-03-30 00:19:36 -0700

    move to aws sdk v3 fix lint fix swagger.json -> specs.json

--- APILambda/lambdatostaging/lambdatostaging.js
+++ APILambda/lambdatostaging/lambdatostaging.js
@@ -1,39 +1,39 @@
-import NpmExecHelper from './lambdatostaginghelper.js';
+import NpmExecHelper from "./lambdatostaginghelper.js"
 
-const npmExecInstance = new NpmExecHelper();
+const npmExecInstance = new NpmExecHelper()
 
 const handleError = (output, errorMessage, dir = "../TiFBackendUtils") => {
-    if (output !== 0) {
-        console.error(errorMessage);
-        console.log(`deleting dist folder content from ${dir}/dist`);
-        cleanUp();
-        process.exit(1);
-    }
-};
+  if (output !== 0) {
+    console.error(errorMessage)
+    console.log(`deleting dist folder content from ${dir}/dist`)
+    cleanUp()
+    process.exit(1)
+  }
+}
 
 const cleanUp = () => {
-    npmExecInstance.revertToPackageJsonName();
-    npmExecInstance.deleteDistBuildFolders();
-};
+  npmExecInstance.revertToPackageJsonName()
+  npmExecInstance.deleteDistBuildFolders()
+}
 
-cleanUp();
+cleanUp()
 
-const cleanInstallOutput = npmExecInstance.cleanInstall();
-handleError(cleanInstallOutput, "Failed to install backend utils dependencies.");
+const cleanInstallOutput = npmExecInstance.cleanInstall()
+handleError(cleanInstallOutput, "Failed to install backend utils dependencies.")
 
-const buildOutput = npmExecInstance.runBuild();
-handleError(buildOutput, "Failed to build backend utils js.");
+const buildOutput = npmExecInstance.runBuild()
+handleError(buildOutput, "Failed to build backend utils js.")
 
-const runPackage = npmExecInstance.runPackageShellJS();
-handleError(runPackage, "Failed to package.");
+const runPackage = npmExecInstance.runPackageShellJS()
+handleError(runPackage, "Failed to package.")
 
-const runInstallAPILambdaDependencies = npmExecInstance.runInstallAPILambdaDependencies();
-handleError(runInstallAPILambdaDependencies, "Failed to install API lambda dependencies.");
+const runInstallAPILambdaDependencies = npmExecInstance.runInstallAPILambdaDependencies()
+handleError(runInstallAPILambdaDependencies, "Failed to install API lambda dependencies.")
 
-const runBuildAPILambdaZip = npmExecInstance.runBuildAPILambdaZip();
-handleError(runBuildAPILambdaZip, "Failed to build APILambda zip.");
+const runBuildAPILambdaZip = npmExecInstance.runBuildAPILambdaZip()
+handleError(runBuildAPILambdaZip, "Failed to build APILambda zip.")
 
-const deployLambdaToStaging = await npmExecInstance.deployLambdaToStagingAWSSdk();
-handleError(deployLambdaToStaging, "Failed to deploy lambda to staging");
+const deployLambdaToStaging = await npmExecInstance.deployLambdaToStagingAWSSdk()
+handleError(deployLambdaToStaging, "Failed to deploy lambda to staging")
 
-cleanUp();
+cleanUp()
--- APILambda/lambdatostaging/lambdatostaginghelper.js
+++ APILambda/lambdatostaging/lambdatostaginghelper.js
@@ -1,323 +1,320 @@
-import fs from "fs";
-import shell from "shelljs";
-import AdmZip from 'adm-zip';
-import AWS from 'aws-sdk';
-import path from 'path';
-import { config } from 'dotenv';
-config();
+import { APIGateway } from "@aws-sdk/client-api-gateway"
+import { Lambda } from "@aws-sdk/client-lambda"
+import { fromEnv } from "@aws-sdk/credential-providers"
+import AdmZip from "adm-zip"
+import { config } from "dotenv"
+import fs from "fs"
+import path from "path"
+import shell from "shelljs"
 
-AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, region: process.env.AWS_REGION || 'us-west-2'
-});
+config()
 
-const lambda = new AWS.Lambda();
-const apigateway = new AWS.APIGateway();
-const TIFBACKENDUTILS_DIR = "../TiFBackendUtils";
-const APILAMBDA_DIR = "../APILambda";
+const lambda = new Lambda({
+  credentials: fromEnv()
+})
+const apigateway = new APIGateway({
+  credentials: fromEnv()
+})
+const TIFBACKENDUTILS_DIR = "../TiFBackendUtils"
+const APILAMBDA_DIR = "../APILambda"
 
 export default class npmExecHelper {
-    deleteDistFolderContent(dir = TIFBACKENDUTILS_DIR) {
-        let result;
+  deleteDistFolderContent (dir = TIFBACKENDUTILS_DIR) {
+    let result
 
-        if (!fs.existsSync(dir)) {
-            console.log(`${dir} does not exist.`);
-            return 1;
-        }
-        const distPath = `${dir}/dist`;
-        const parentDir = path.basename(path.dirname(distPath));
+    if (!fs.existsSync(dir)) {
+      console.log(`${dir} does not exist.`)
+      return 1
+    }
+    const distPath = `${dir}/dist`
+    const parentDir = path.basename(path.dirname(distPath))
 
-        if (parentDir !== 'APILambda' && parentDir !== 'TiFBackendUtils') {
-            console.log(`The parent directory of 'dist' folder should be either 'APILambda' or 'TiFBackendUtils'.`);
-            return 1;
-        }
-
-        console.log(`Deleting all contents from ${distPath}`);
-        result = shell.rm('-rf', distPath);
-        if (this.handleShellError(result)) {
-            return result.code;
-        }
-        console.log(`Deleted contents successfully from ${distPath}`);
-
-        if (fs.existsSync(`${distPath}.zip`)) {
-            console.log(`Deleting ${distPath}.zip`);
-            result = shell.rm('-rf', `${distPath}.zip`);
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-            console.log(`Deleted ${distPath}.zip successfully`);
-        }
+    if (parentDir !== "APILambda" && parentDir !== "TiFBackendUtils") {
+      console.log("The parent directory of 'dist' folder should be either 'APILambda' or 'TiFBackendUtils'.")
+      return 1
     }
 
-    deleteDistBuildFolders() {
-        this.deleteDistFolderContent(TIFBACKENDUTILS_DIR);
-        this.deleteDistFolderContent(APILAMBDA_DIR);
+    console.log(`Deleting all contents from ${distPath}`)
+    result = shell.rm("-rf", distPath)
+    if (this.handleShellError(result)) {
+      return result.code
     }
+    console.log(`Deleted contents successfully from ${distPath}`)
 
-    handleShellError(result) {
-        if (result.code !== 0) {
-            console.error("Error:", result.stderr);
-            return 1;
+    if (fs.existsSync(`${distPath}.zip`)) {
+      console.log(`Deleting ${distPath}.zip`)
+      result = shell.rm("-rf", `${distPath}.zip`)
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+      console.log(`Deleted ${distPath}.zip successfully`)
+    }
+  }
+
+  deleteDistBuildFolders () {
+    this.deleteDistFolderContent(TIFBACKENDUTILS_DIR)
+    this.deleteDistFolderContent(APILAMBDA_DIR)
+  }
+
+  handleShellError (result) {
+    if (result.code !== 0) {
+      console.error("Error:", result.stderr)
+      return 1
+    }
+    return 0
+  }
+
+  revertToPackageJsonName (dir = TIFBACKENDUTILS_DIR) {
+    try {
+      if (!fs.existsSync(`${dir}/original-package.json`)) {
+        console.log(`${dir}/original-package.json does not exist`)
+        return
+      }
+
+      fs.unlinkSync(`${dir}/package.json`)
+      fs.renameSync(`${dir}/original-package.json`, `${dir}/package.json`)
+    } catch (error) {
+      console.log("Error:", error)
+    }
+  }
+
+  removePatchPackage () {
+    const packageJsonTemp = JSON.parse(fs.readFileSync("package.json", "utf8"))
+    delete packageJsonTemp.scripts.postinstall
+    fs.writeFileSync("package.json", JSON.stringify(packageJsonTemp, null, 2))
+  }
+
+  cleanInstall () {
+    try {
+      console.log("1. Installing backend utils dependencies")
+
+      let result = shell.cd(TIFBACKENDUTILS_DIR)
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+
+      fs.copyFileSync("package.json", "original-package.json")
+
+      this.removePatchPackage()
+      result = shell.exec("npm ci --omit=dev")
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+
+      return 0
+    } catch (error) {
+      console.log("Error:", error)
+      return 1
+    }
+  }
+
+  runBuild () {
+    try {
+      console.log("2. Building backend utils js")
+
+      let result = shell.cd(TIFBACKENDUTILS_DIR)
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+      result = shell.exec("npm run build")
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+      return 0
+    } catch (error) {
+      console.log("Error:", error)
+      return 1
+    }
+  }
+
+  runPackageShellJS () {
+    try {
+      console.log("3. Building package")
+
+      let result = shell.cd(TIFBACKENDUTILS_DIR)
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+
+      shell.config.globOptions.extglob = true
+
+      result = shell.cp("-R", ["!(test|.github|dist|*.ts)", "dist/"])
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+      result = shell.cd("./dist")
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+      result = shell.exec("npm pack")
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+      result = shell.cd("..")
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+
+      return 0
+    } catch (error) {
+      console.log("Error:", error)
+      return 1
+    }
+  }
+
+  runInstallAPILambdaDependencies () {
+    try {
+      console.log("4. Installing api lambda dependencies")
+
+      let result = shell.cd("../GeocodingLambda")
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+
+      result = shell.exec("npm ci --omit=dev")
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+
+      result = shell.cd("../APILambda")
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+
+      result = shell.exec("npm ci")
+
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+
+      result = shell.exec("npm ci ../TiFBackendUtils/dist/TiFBackendUtils-1.0.0.tgz")
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+
+      return 0
+    } catch (error) {
+      console.error("Error:", error)
+      return 1
+    }
+  }
+
+  runBuildAPILambdaZip () {
+    try {
+      console.log("5. Run build API Lambda zip")
+
+      let result = shell.cd(APILAMBDA_DIR)
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+      result = shell.exec("npm run build")
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+
+      result = shell.exec("npm ci --omit=dev")
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+
+      shell.config.globOptions.extglob = true
+
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+
+      result = shell.cp("-R", ["!(test|.github|dist|lambdatostaging|*.ts)", "dist/"])
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+      result = shell.cd("dist")
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+
+      result = shell.rm("-rf", "test")
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+
+      const zip = new AdmZip()
+      zip.addLocalFolder(".", "dist")
+      zip.writeZip("../dist.zip", (err) => {
+        if (err) {
+          console.error("Error writing zip file:", err)
+          return 1
+        } else {
+          console.log("Zip file created successfully.")
         }
-        return 0;
+      })
+
+      result = shell.cd("..")
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+
+      result = shell.exec("npm ci")
+      if (this.handleShellError(result)) {
+        return result.code
+      }
+      return 0
+    } catch (error) {
+      console.error("Error:", error)
+      return 1
     }
-    revertToPackageJsonName(dir = TIFBACKENDUTILS_DIR) {
-        try {
-            if (!fs.existsSync(`${dir}/original-package.json`)) {
-                console.log(`${dir}/original-package.json does not exist`);
-                return;
-            }
+  }
 
-            fs.unlinkSync(`${dir}/package.json`);
-            fs.renameSync(`${dir}/original-package.json`, `${dir}/package.json`);
-        } catch (error) {
-            console.log("Error:", error);
-        }
+  async deployLambdaToStagingAWSSdk () {
+    try {
+      console.log("6. Deploy lambda to staging")
+      const stageName = "staging"
+
+      const updateFunctionCodeParams = {
+        FunctionName: "lambdaSQLRoute",
+        ZipFile: fs.readFileSync("dist.zip")
+      }
+
+      const putRestApiParams = {
+        restApiId: "623qsegfb9",
+        failOnWarnings: true,
+        mode: "overwrite",
+        body: fs.readFileSync("specs.json")
+      }
+
+      const createDeploymentParams = {
+        restApiId: "623qsegfb9",
+        stageName,
+        description: "Deployed"
+      }
+
+      try {
+        await lambda.updateFunctionCode(updateFunctionCodeParams).promise()
+        console.log("Successfully updated lambda")
+      } catch (err) {
+        console.error(err)
+        return 1
+      }
+
+      try {
+        await apigateway.putRestApi(putRestApiParams).promise()
+        console.log("Successfully added spec file")
+      } catch (err) {
+        console.error(err)
+        return 1
+      }
+
+      try {
+        await apigateway.createDeployment(createDeploymentParams).promise()
+        console.log("Successfully deployed")
+      } catch (err) {
+        console.error(err)
+        return 1
+      }
+
+      return 0
+    } catch (error) {
+      console.log("Error:", error)
+      return 1
     }
-
-    removePatchPackage() {
-        const packageJsonTemp = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-        delete packageJsonTemp.scripts.postinstall;
-        fs.writeFileSync('package.json', JSON.stringify(packageJsonTemp, null, 2));
-    }
-
-    cleanInstall() {
-        try {
-            console.log("1. Installing backend utils dependencies");
-
-            let result = shell.cd(TIFBACKENDUTILS_DIR);
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-
-            fs.copyFileSync('package.json', 'original-package.json');
-
-            this.removePatchPackage();
-            result = shell.exec('npm ci --omit=dev');
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-
-            return 0;
-        } catch (error) {
-            console.log("Error:", error);
-            return 1;
-        }
-    }
-
-    runBuild() {
-        try {
-            console.log("2. Building backend utils js");
-
-            let result = shell.cd(TIFBACKENDUTILS_DIR);
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-            result = shell.exec('npm run build');
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-            return 0;
-        } catch (error) {
-            console.log("Error:", error);
-            return 1;
-        }
-    }
-
-    runPackageShellJS() {
-        try {
-
-            console.log("3. Building package");
-
-            let result = shell.cd(TIFBACKENDUTILS_DIR);
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-
-            shell.config.globOptions.extglob = true;
-
-            result = shell.cp('-R', ['!(test|.github|dist|*.ts)', 'dist/']);
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-            result = shell.cd('./dist');
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-            result = shell.exec('npm pack');
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-            result = shell.cd('..');
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-
-            return 0;
-        }
-
-        catch (error) {
-            console.log("Error:", error);
-            return 1;
-        }
-    }
-
-    runInstallAPILambdaDependencies() {
-        try {
-            console.log("4. Installing api lambda dependencies");
-
-            let result = shell.cd('../GeocodingLambda');
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-
-            result = shell.exec('npm ci --omit=dev');
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-
-            result = shell.cd('../APILambda');
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-
-            result = shell.exec('npm ci');
-
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-
-            result = shell.exec('npm ci ../TiFBackendUtils/dist/TiFBackendUtils-1.0.0.tgz');
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-
-            return 0;
-        } catch (error) {
-            console.error('Error:', error);
-            return 1;
-        }
-    }
-
-    runBuildAPILambdaZip() {
-        try {
-
-            console.log("5. Run build API Lambda zip");
-
-            let result = shell.cd(APILAMBDA_DIR);
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-            result = shell.exec('npm run build');
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-
-            result = shell.exec('npm ci --omit=dev');
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-
-            shell.config.globOptions.extglob = true;
-
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-
-            result = shell.cp('-R', ['!(test|.github|dist|lambdatostaging|*.ts)', 'dist/']);
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-            result = shell.cd("dist");
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-
-            result = shell.rm('-rf', 'test');
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-
-            const zip = new AdmZip();
-            zip.addLocalFolder('.', 'dist');
-            zip.writeZip('../dist.zip', (err) => {
-                if (err) {
-                    console.error('Error writing zip file:', err);
-                    return 1;
-                } else {
-                    console.log('Zip file created successfully.');
-                }
-            });
-
-            result = shell.cd("..");
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-
-            result = shell.exec('npm ci');
-            if (this.handleShellError(result)) {
-                return result.code;
-            }
-            return 0;
-
-        } catch (error) {
-            console.error('Error:', error);
-            return 1;
-        }
-    }
-
-    async deployLambdaToStagingAWSSdk() {
-        try {
-
-            console.log("6. Deploy lambda to staging");
-            const stageName = "staging";
-
-            const updateFunctionCodeParams = {
-                FunctionName: 'lambdaSQLRoute',
-                ZipFile: fs.readFileSync('dist.zip')
-            };
-
-            const putRestApiParams = {
-                restApiId: '623qsegfb9',
-                failOnWarnings: true,
-                mode: 'overwrite',
-                body: fs.readFileSync('swagger.json')
-            };
-
-            const createDeploymentParams = {
-                restApiId: '623qsegfb9',
-                stageName: stageName,
-                description: 'Deployed'
-            };
-
-            try {
-                await lambda.updateFunctionCode(updateFunctionCodeParams).promise();
-                console.log("Successfully updated lambda");
-            } catch (err) {
-                console.error(err);
-                return 1;
-            }
-
-            try {
-                await apigateway.putRestApi(putRestApiParams).promise();
-                console.log("Successfully added spec file");
-            } catch (err) {
-                console.error(err);
-                return 1;
-            }
-
-            try {
-                await apigateway.createDeployment(createDeploymentParams).promise();
-                console.log("Successfully deployed");
-            } catch (err) {
-                console.error(err);
-                return 1;
-            }
-
-            return 0;
-
-        } catch (error) {
-            console.log("Error:", error);
-            return 1;
-        }
-    }
+  }
 }
