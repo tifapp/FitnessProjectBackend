9da9f63bc8879d9b3ca828314a1810fc502b72ce
suryatiru <sutiruma@ucsc.edu>
2024-04-25 00:04:49 -0700

    Generated SQL tables

---
+++ APILambda/test/helpers/createTables.ts
@@ -0,0 +1,23 @@
+const createEventSQL = `CREATE TABLE IF NOT EXISTS event (
+    id bigint NOT NULL AUTO_INCREMENT,
+    description varchar(1000) NOT NULL,
+    startDateTime datetime NOT NULL,
+    endDateTime datetime NOT NULL,
+    hostId char(36) NOT NULL,
+    color varchar(9) NOT NULL,
+    title varchar(100) NOT NULL,
+    shouldHideAfterStartDate tinyint(1) NOT NULL,
+    isChatEnabled tinyint(1) NOT NULL,
+    latitude decimal(10,7) NOT NULL,
+    longitude decimal(10,7) NOT NULL,
+    createdDateTime datetime NOT NULL DEFAULT current_timestamp(),
+    updatedDateTime datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+    endedDateTime datetime,
+    PRIMARY KEY (id),
+    UNIQUE KEY unique_event_id (id),
+    KEY event_host_must_exist (hostId),
+    KEY idx_startDateTime (startDateTime),
+    CONSTRAINT event_host_must_exist FOREIGN KEY (hostId) REFERENCES user (id),
+    CONSTRAINT event_must_end_after_start CHECK (startDateTime < endDateTime),
+    CONSTRAINT format_event_color CHECK (REGEXP_LIKE(color, _utf8mb4 '^#[0-9A-Fa-f]{6}$'))
+)`
