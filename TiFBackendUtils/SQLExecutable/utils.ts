474a11500f648797a6109b3a785f424b268004ee
tifapp <tifapppublic@gmail.com>
2024-04-24 21:33:07 -0700

    try aurora integration

--- TiFBackendUtils/SQLExecutable/utils.ts
+++ TiFBackendUtils/SQLExecutable/utils.ts
@@ -1,7 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // TODO: Replace with backend utils
-import { Connection } from "@planetscale/database"
-import { AwaitableResult, failure, promiseResult, success } from "../result.js"
+import { BeginTransactionCommand, CommitTransactionCommand, ExecuteStatementCommand, Field, RDSDataClient, RollbackTransactionCommand, SqlParameter } from "@aws-sdk/client-rds-data"
+import { envVars } from "../Planetscale/env.js"
+import { AnyResult, PromiseResult, failure, promiseResult, success } from "../result.js"
+
+const dbEnv = {
+  resourceArn: "arn:aws:rds:region:accountId:cluster:clusterIdentifier", // Your cluster ARN (use env)
+  secretArn: "arn:aws:secretsmanager:region:accountId:secret:secretName", // Your secret ARN for database credentials (use env)
+  database: envVars.DATABASE_NAME
+}
 
 /**
  * An interface for performing commonly used operations on a SQL database
@@ -16,11 +23,41 @@ type QueryResult = {
 }
 
 export class SQLExecutable {
-  private conn: Connection // Define the appropriate type for your database connection
+  private client: RDSDataClient
 
-  constructor (connection: Connection) {
-    // Use the appropriate type for the connection
-    this.conn = connection
+  constructor (client: RDSDataClient) {
+    this.client = client
+  }
+
+  private async execute<Value> (sql: string, args: object | null = null): Promise<Value[]> {
+    const parameters: SqlParameter[] = args
+      ? Object.entries(args).map(([key, value]) => {
+      // Determine the type of the value to assign the correct field (e.g., stringValue, blobValue, etc.)
+        let paramValue: Field
+        if (typeof value === "string") {
+          paramValue = { stringValue: value }
+        } else if (typeof value === "number") {
+          paramValue = Number.isInteger(value) ? { longValue: value } : { doubleValue: value }
+        } else if (typeof value === "boolean") {
+          paramValue = { booleanValue: value }
+        } else if (value instanceof Buffer) {
+          paramValue = { blobValue: value }
+        } else {
+          throw new Error(`Unsupported type for parameter ${key}`)
+        }
+
+        return { name: key, value: paramValue }
+      })
+      : []
+
+    const command = new ExecuteStatementCommand({
+      ...dbEnv,
+      sql,
+      parameters
+    })
+
+    const result = await this.client.send(command)
+    return result.records as Value[]
   }
 
   // ==================
@@ -39,24 +76,36 @@ export class SQLExecutable {
    * ```
    */
 
-  private async execute<Value> (
-    query: string,
-    args: object | any[] | null = null
-  ): Promise<Value[]> {
-    // Use this.conn to execute the query and return the result rows
-    // This will be the only function to directly use the database library's execute method.
-    const result = await this.conn.execute(query, args)
-    return result.rows as Value[]
-  }
+  private async executeAndReturnQueryResult (sql: string, args: object | null = null): Promise<QueryResult> {
+    const parameters: SqlParameter[] = args
+      ? Object.entries(args).map(([key, value]) => {
+      // Determine the type of the value to assign the correct field (e.g., stringValue, blobValue, etc.)
+        let paramValue: Field
+        if (typeof value === "string") {
+          paramValue = { stringValue: value }
+        } else if (typeof value === "number") {
+          paramValue = Number.isInteger(value) ? { longValue: value } : { doubleValue: value }
+        } else if (typeof value === "boolean") {
+          paramValue = { booleanValue: value }
+        } else if (value instanceof Buffer) {
+          paramValue = { blobValue: value }
+        } else {
+          throw new Error(`Unsupported type for parameter ${key}`)
+        }
 
-  private async executeAndReturnQueryResult (
-    query: string,
-    args: object | any[] | null = null
-  ): Promise<QueryResult> {
-    // Use this.conn to execute the query and return the result rows
-    // This will be the only function to directly use the database library's execute method.
-    const result = await this.conn.execute(query, args)
-    return { ...result }
+        return { name: key, value: paramValue }
+      })
+      : []
+
+    const command = new ExecuteStatementCommand({
+      ...dbEnv,
+      sql,
+      parameters
+    })
+
+    const result = await this.client.send(command)
+
+    return { rowsAffected: result.numberOfRecordsUpdated ?? 0, insertId: `${result.generatedFields?.map(field => field.longValue)[0]}` ?? "" }
   }
 
   /**
@@ -82,10 +131,51 @@ export class SQLExecutable {
   /**
    * Performs an idempotent transaction and returns the result of the transaction wrapped in a {@link PromiseResult}.
    */
+  // transaction<SuccessValue, ErrorValue> (
+  //   query: (tx: SQLExecutable) => AwaitableResult<SuccessValue, ErrorValue>
+  // ) {
+  //   return promiseResult(this.transaction(async () => query(this)))
+  // }
+
+  private beginTransaction (): PromiseResult<string, void> {
+    return promiseResult(
+      this.client.send(new BeginTransactionCommand({
+        ...dbEnv
+      })).then(response => response.transactionId ? success(response.transactionId) : failure())
+    )
+  }
+
+  private endTransaction (transactionId: string): PromiseResult<void, Error> {
+    return promiseResult(this.client.send(new CommitTransactionCommand({
+      ...dbEnv,
+      transactionId
+    }))
+      .then(() => success())
+      .catch(error => failure(error))
+    )
+  }
+
+  private rollbackTransaction (transactionId: string, error: unknown): PromiseResult<never, unknown> {
+    return promiseResult(
+      this.client.send(new RollbackTransactionCommand({
+        ...dbEnv,
+        transactionId
+      })).then(() => failure(error))
+    )
+  }
+
   transaction<SuccessValue, ErrorValue> (
-    query: (tx: SQLExecutable) => AwaitableResult<SuccessValue, ErrorValue>
+    query: (tx: SQLExecutable) => AnyResult<SuccessValue, ErrorValue>
   ) {
-    return promiseResult(this.conn.transaction(async () => query(this)))
+    return this.beginTransaction()
+      .flatMapSuccess(transactionId =>
+        query(this)
+          .flatMapSuccess(
+            (successValue) => this.endTransaction(transactionId).mapSuccess(() => successValue)
+          ).flatMapFailure(error => this.rollbackTransaction(transactionId, error))
+      ).mapFailure(
+        error => error
+      )
   }
 
   // ==================
