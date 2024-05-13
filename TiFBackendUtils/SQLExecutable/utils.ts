de8175bc52b7e960bebf9935a4a954e059073408
suryatiru <sutiruma@ucsc.edu>
2024-05-13 15:40:40 -0700

    Create SQLExecutable for PlanetScale
    - Create an interface that will consist of methods and properties that other db providers will have different implementations for
    - For each new db provider, we will need to have a separate sqlexecutable that will implement that interface
    - We pass in that sqlexecutable to the utils file that contain the remaining methods and properties that is consistent with all db providers

--- TiFBackendUtils/SQLExecutable/utils.ts
+++ TiFBackendUtils/SQLExecutable/utils.ts
@@ -1,62 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // TODO: Replace with backend utils
-import { Connection } from "@planetscale/database"
-import { AwaitableResult, failure, promiseResult, success } from "../result.js"
-
-/**
- * An interface for performing commonly used operations on a SQL database
- *
- * This interface allows for isolated query functions to be made and used in different contexts,
- * transaction or not.
- */
-
-type QueryResult = {
-  insertId: string
-  rowsAffected: number
-}
+// import { Connection } from "@planetscale/database"
+import { failure, promiseResult, success } from "../result.js"
+import { SQLDriverInterface } from "./SQLDriverInterface.js"
 
 export class SQLExecutable {
-  private conn: Connection // Define the appropriate type for your database connection
+  private driver: SQLDriverInterface // Define the appropriate type for your database connection
 
-  constructor (connection: Connection) {
+  constructor (driver: SQLDriverInterface) {
     // Use the appropriate type for the connection
-    this.conn = connection
-  }
-
-  // ==================
-  // Implementation-Dependent Methods
-  // ==================
-
-  /**
-   * Loads a list of results from the database given a sql query and casts the result to `Value`
-   *
-   * @example
-   * ```ts
-   * type User = { id: string, ... }
-   *
-   * const results = await execute<User>(conn, "SELECT * FROM user");
-   * console.log(results[0].id) // ✅ Typesafe
-   * ```
-   */
-
-  private async execute<Value> (
-    query: string,
-    args: object | any[] | null = null
-  ): Promise<Value[]> {
-    // Use this.conn to execute the query and return the result rows
-    // This will be the only function to directly use the database library's execute method.
-    const result = await this.conn.execute(query, args)
-    return result.rows as Value[]
-  }
-
-  private async executeAndReturnQueryResult (
-    query: string,
-    args: object | any[] | null = null
-  ): Promise<QueryResult> {
-    // Use this.conn to execute the query and return the result rows
-    // This will be the only function to directly use the database library's execute method.
-    const result = await this.conn.execute(query, args)
-    return { ...result }
+    this.driver = driver
   }
 
   /**
@@ -64,7 +17,7 @@ export class SQLExecutable {
    */
   queryResult (query: string, args: object | any[] | null = null) {
     return promiseResult(
-      this.executeAndReturnQueryResult(query, args).then((queryResult) =>
+      this.driver.executeAndReturnQueryResult(query, args).then((queryResult) =>
         success(queryResult)
       )
     )
@@ -75,17 +28,8 @@ export class SQLExecutable {
    */
   queryResults<Value> (query: string, args: object | any[] | null = null) {
     return promiseResult(
-      this.execute<Value>(query, args).then((result) => success(result))
+      this.driver.execute<Value>(query, args).then((result) => success(result))
     )
-  }
-
-  /**
-   * Performs an idempotent transaction and returns the result of the transaction wrapped in a {@link PromiseResult}.
-   */
-  transaction<SuccessValue, ErrorValue> (
-    query: (tx: SQLExecutable) => AwaitableResult<SuccessValue, ErrorValue>
-  ) {
-    return promiseResult(this.conn.transaction(async () => query(this)))
   }
 
   // ==================
@@ -100,7 +44,7 @@ export class SQLExecutable {
    */
   queryHasResults (query: string, args: object | any[] | null = null) {
     return promiseResult(
-      this.execute(query, args).then((results) => {
+      this.driver.execute(query, args).then((results) => {
         const hasResults = results.length > 0
         return hasResults ? success(hasResults) : failure(hasResults)
       })
@@ -120,14 +64,9 @@ export class SQLExecutable {
    */
   queryFirstResult<Value> (query: string, args: object | any[] | null = null) {
     return promiseResult(
-      this.execute<Value>(query, args).then((results) =>
+      this.driver.execute<Value>(query, args).then((results) =>
         results[0] ? success(results[0]) : failure("no-results" as const)
       )
     )
   }
 }
-
-// Usage
-// const dbConnection = ...;  // Initialize or import your database connection
-// const sqlExec = new SQLExecutable(dbConnection);
-// sqlExec.queryHasResults("SELECT * FROM users");
