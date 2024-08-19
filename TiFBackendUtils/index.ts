b90e46d7542e0afd2a48fc8abb7aeafdd1cc9654
tifapp <tifapppublic@gmail.com>
2024-08-18 22:07:45 -0700

    have github action build all projects + simplify tsconfigs

--- TiFBackendUtils/index.ts
+++ TiFBackendUtils/index.ts
@@ -1,8 +1,8 @@
 export * from "./AWS/index.js"
-export * from "./MySQLDriver/index.js"
-export * from "./Retryable/utils.js"
-export * from "./TiFUserUtils/index.js"
-export * from "./TifEventUtils.js"
 export * from "./entities.js"
 export * from "./env.js"
+export * from "./MySQLDriver/index.js"
+export * from "./Retryable/utils.js"
+export * from "./TifEventUtils.js"
+export * from "./TiFUserUtils/index.js"
 
