e5df476560a8180a861ddf18a424319ef2b1c05c
mhayes853 <matthewha853@gmail.com>
2024-11-13 21:56:01 -0800

    Fix errors

--- TiFBackendUtils/MySQLDriver/paramify.ts
+++ TiFBackendUtils/MySQLDriver/paramify.ts
@@ -1,17 +1,15 @@
+import { DatabaseValueConvertible } from "TiFShared/lib/Database"
+
 export type SQLParams =
   | undefined
-  | Record<string, unknown>
+  | Record<string, number | string | DatabaseValueConvertible>
   | (number | string)[]
 
-const paramify = (value: unknown) =>
+const paramify = (value: number | string | DatabaseValueConvertible) =>
   value == null
     ? null
-    : value.constructor.name === "UserHandle" ||
-      value.constructor.name === "ColorString"
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore .toJSON() here causes a build issue for some reason
-    // NB: instanceof checks don't work for UserHandle/ColorString, possibly due to peer dependencies
-      ? value.toJSON()
+    : typeof value === "object" && "toDatabaseValue" in value
+      ? value.toDatabaseValue()
       : value
 
 export const paramifyArgs = (args: SQLParams) =>
