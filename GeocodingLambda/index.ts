e1388d6aa0482f9b4e4ad57b7d251c84107af84c
tifapp <tifapppublic@gmail.com>
2024-12-28 00:07:47 -0800

    try more logs

--- GeocodingLambda/index.ts
+++ GeocodingLambda/index.ts
@@ -1,14 +1,11 @@
-try {
-  const { handler } = require("./handler")
-  exports.handler = async (...rest: unknown[]) => {
-    try {
-      return (await handler(...rest)).unwrap()
-    } catch (error: unknown) {
-      console.error("Execution error:", error)
-      throw error
-    }
+exports.handler = async (event: any, context: any) => {
+  console.log("Log Stream Name:", context.logStreamName)
+  console.log("Event:", JSON.stringify(event))
+  try {
+    const { handler } = require("./handler")
+    return (await handler(event, context)).unwrap()
+  } catch (error) {
+    console.error("Execution error:", error)
+    throw error
   }
-} catch (error) {
-  console.error("Import error:", error)
-  throw error
 }
