b9872d70121168249b0fac9077d8cf17a614211a
ZoroarkZark <ilikepie1464@gmail.com>
2025-01-14 22:24:18 -0800

    SQL query change to switch join

--- TiFBackendUtils/TiFEventUtils/UserEvent.ts
+++ TiFBackendUtils/TiFEventUtils/UserEvent.ts
@@ -20,7 +20,7 @@ export namespace UserEventSQL {
       ON UserRelationOfUserToHost.fromUserId = :userId AND UserRelationOfUserToHost.toUserId = TifEventView.hostId
       `
   export const ATTENDANCE_INNER_JOIN =
-    "INNER JOIN eventAttendance ea ON ea.userId = :attendingUserId"
+    "INNER JOIN eventAttendance ea ON ea.eventId = TifEventView.id"
 
   const BASE_WHERE_CLAUSES = `
     TifEventView.endDateTime > NOW()
@@ -32,6 +32,13 @@ export namespace UserEventSQL {
     WHERE
       ${BASE_WHERE_CLAUSES}
     `
+
+  export const USER_ATTENDANCE_WHERE = `
+    WHERE
+    ${BASE_WHERE_CLAUSES}
+    AND ea.userId = :attendingUserId
+    AND ea.role IN ('hosting', 'attending')
+  `
   export const GEOSPATIAL_WHERE = `
     WHERE
       ST_Distance_Sphere(POINT(:userLongitude, :userLatitude), POINT(TifEventView.longitude, TifEventView.latitude)) < :radius
